746ed1a235e3889365d5305e115930fce50d3628
SamuelLopezF <Samuellopez.f@gmail.com>
2023-10-29 13:41:39 -0400

    Connecting Front-end login and sign up functions to FirebaseAuth services. Controlling the main page button to disappear corresponding to login status

--- ClassroomAttendanceTracker/app/src/main/java/com/example/classroomattendancetracker/ScanNFCActivitySignIn.java
+++ ClassroomAttendanceTracker/app/src/main/java/com/example/classroomattendancetracker/ScanNFCActivitySignIn.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +27,9 @@ public class ScanNFCActivitySignIn extends AppCompatActivity {
     TextView textViewError;
     TextView textViewSuccess;
 
+    FirebaseUser user;
+    String email;
+
     boolean tried_to_sign_in;
 
     @Override
@@ -39,6 +44,13 @@ public class ScanNFCActivitySignIn extends AppCompatActivity {
         imageViewSuccess = findViewById(R.id.imageViewSuccess);
         textViewError = findViewById(R.id.textViewError);
         textViewSuccess = findViewById(R.id.textViewSuccess);
+
+        if(user != null){
+            email = user.getEmail();
+        }else{
+            email = "null";
+        }
+        Log.d("SCAN_NFC_EMAIL" , email);
 
         tried_to_sign_in = false;
         refresh();
