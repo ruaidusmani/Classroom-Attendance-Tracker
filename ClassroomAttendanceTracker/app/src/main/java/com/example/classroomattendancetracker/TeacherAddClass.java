e3ca9414d18bcce623c6dfc5ea0c51da4b972c59
ruaid_usmani <ruaidusmani@gmail.com>
2023-11-27 00:17:17 -0500

    Implemented spinners that selects building&classroom to store in db

--- ClassroomAttendanceTracker/app/src/main/java/com/example/classroomattendancetracker/TeacherAddClass.java
+++ ClassroomAttendanceTracker/app/src/main/java/com/example/classroomattendancetracker/TeacherAddClass.java
@@ -63,10 +63,11 @@ public class TeacherAddClass extends AppCompatActivity implements AdapterView.On
     ArrayList<String> subjects = new ArrayList<> (Arrays.asList("COEN", "ELEC", "AERO", "MECH"));
 
     //List of possible buildings
-
     HashMap<String, List<String>> Map_Building_Rooms = new HashMap<String, List<String>>();
-    ArrayList<String> buildings = new ArrayList<>(Arrays.asList("H", "MB", "EV", "FB", "FG", "LS"));
-    String building;
+    String selected_building;
+    String selected_class;
+
+    String room; //room number
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,66 +101,149 @@ public class TeacherAddClass extends AppCompatActivity implements AdapterView.On
         chipGroup = findViewById(R.id.chip_days_group);
 
 
-        //Spinner init for selecting building
-        building_spinner = findViewById(R.id.building_spinner);
-        ArrayAdapter<CharSequence> building_adapter = ArrayAdapter.createFromResource(this,
-                R.array.building_array, R.layout.building_spinner_text);
-        building_adapter.setDropDownViewResource(R.layout.building_spinner_dropdown);
-        building_spinner.setAdapter(building_adapter);
 
+        //Spinner init for selecting selected_building
+        building_spinner = findViewById(R.id.building_spinner);
         //Spinner init for selecting rooms
         class_spinner = findViewById(R.id.class_spinner);
 
         Populate_Map_Building_Rooms();
 
+
+        ArrayAdapter<CharSequence> building_adapter = ArrayAdapter.createFromResource(this,
+                R.array.building_array, R.layout.building_spinner_text);
+        building_adapter.setDropDownViewResource(R.layout.building_spinner_dropdown);
+        building_spinner.setAdapter(building_adapter);
+        building_spinner.setSelection(0); // "Please Select" will be the default selection
+
         building_spinner.setOnItemSelectedListener(this);
+        class_spinner.setOnItemSelectedListener(this);
 
-        // Set up the classSpinner listener
-        class_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedClass = parent.getItemAtPosition(position).toString();
-                Toast.makeText(parent.getContext(), selectedClass, Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing
-            }
-        });
 
         //Adding Listeners
         submit_class.setOnClickListener(ActivityClickListener);
     }
 
     public void Populate_Map_Building_Rooms(){
-        Map_Building_Rooms.put("H", Arrays.asList("Class H1", "Class H2", "Class H3"));
-        Map_Building_Rooms.put("MB", Arrays.asList("Class MB1", "Class MB2", "Class MB3"));
-        Map_Building_Rooms.put("EV", Arrays.asList("Class EV1", "Class EV2", "Class EV3"));
+        String[] H_rooms = {
+                "501", "507", "509", "513", "520", "521", "529",
+                "531", "535","537", "539", "540", "544", "553",
+                "557", "561", "562", "564", "565",
+                "601", "605", "607", "609", "613", "620", "621",
+                "625", "670",
+                "820",
+                "920", "937",
+                "1070", "1011",
+                "1252", "1254-02", "1269", "1267", "1271"
+        };
+
+        String[] MB_rooms = {
+                "S2.105", "S2.115", "S2.210", "S2.285", "S2.330", "S2.401", "S2.445",
+                "S2.455", "S2.465",
+                "S1.105", "S1.115", "S1.1235", "S1.1255", "S1.1401", "S1.430", "S1.435",
+                "1.210", "1.301", "1.437",
+                "2.210", "2.255", "2.265", "2.270", "2.285",
+                "3.210", "3.255", "3.265", "3.270", "3.285",
+                "3.430", "3.435", "3.445",
+                "4.206",
+                "5.255", "5.265", "5.275",
+                "6.240", "6.260", "6.425"
+        };
+
+        String[] LS_rooms = {
+                "105", "107", "108", "110",
+                "205", "207", "208", "210"
+        };
+
+        String[] FB_rooms = {
+                "S-109", "S-113", "S-129",
+                "S-133", "S-143", "S-150"
+        };
+
+        String[] FG_rooms = {
+                "B-030", "B-040", "B-050", "B-060", "B-070", "B-080",
+                "C-070", "C-080"
+        };
+
+        String[] please_select = {"Please Select"};
+
+        Map_Building_Rooms.put("Please Select", Arrays.asList(please_select));
+        Map_Building_Rooms.put("H", Arrays.asList(H_rooms));
+        Map_Building_Rooms.put("MB", Arrays.asList(MB_rooms));
+        Map_Building_Rooms.put("FB", Arrays.asList(FB_rooms));
+        Map_Building_Rooms.put("FG", Arrays.asList(FG_rooms));
+        Map_Building_Rooms.put("LS", Arrays.asList(LS_rooms));
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        building = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(), adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+        if(adapterView.getId() == R.id.building_spinner)
+        {
+            if (!adapterView.getItemAtPosition(i).toString().equals("Please Select")) {
+                selected_building = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(adapterView.getContext(), adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
 
-        // Get classes for the selected building from the HashMap
-        List<String> classes = Map_Building_Rooms.get(building);
+                // Get classes for the selected selected_building from the HashMap
+                List<String> classes = Map_Building_Rooms.get(selected_building);
 
-        // Create an ArrayAdapter for classes based on the selected building
-        ArrayAdapter<String> classAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, classes);
 
-        // Specify the layout to use when the list of choices appears
-        classAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                if (classes != null) {
+                    List<String> modifiableClasses = new ArrayList<>(classes);
+                    modifiableClasses.add(0, "Please Select");
+                    ArrayAdapter<String> classAdapter = new ArrayAdapter<>(this,
+                            R.layout.building_spinner_text, modifiableClasses);
+                    classAdapter.setDropDownViewResource(R.layout.building_spinner_dropdown);
+                    class_spinner.setAdapter(classAdapter);
+                    class_spinner.setSelection(0);
+                    doSomethingWithSelectedBuilding(selected_building);
+                } else {
+                    // Handle the case when no rooms are available for the selected building
+                    // For instance, you might clear the class_spinner or display a message
+                    class_spinner.setAdapter(null); // Clear the adapter or set to default
+                    Toast.makeText(adapterView.getContext(), "No rooms available", Toast.LENGTH_SHORT).show();
+                    doSomethingWithSelectedBuilding(selected_building);
+                }
 
-        // Apply the adapter to the classSpinner
-        class_spinner.setAdapter(classAdapter);
+                if (selected_class != null && !selected_class.equals("Please Select")) {
+                    room = selected_building + " " + selected_class;
+                    Log.d("Room value 1", room); // Log the room value for verification
+                    update_room(selected_building, selected_class);
+                }
+
+
+            } else {}
+        }
+        else if(adapterView.getId() == R.id.class_spinner)
+        {
+            if (!adapterView.getItemAtPosition(i).toString().equals("Please Select")){
+                selected_class = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(adapterView.getContext(), selected_class, Toast.LENGTH_SHORT).show();
+                //room = selected_building + " " + selected_class;
+
+                if (selected_building != null && !selected_building.equals("Please Select")) {
+                    room = selected_building + " " + selected_class;
+                    Log.d("Room value 2", room); // Log the room value for verification
+                    update_room(selected_building, selected_class);
+                }
+            }
+            else{}
+        }
 
     }
 
+    public void update_room(String selected_building, String selected_class){
+        room = selected_building + " " + selected_class;
+    }
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {}
+
+    public void doSomethingWithSelectedBuilding(String mew_selected_building){
+        this.selected_building = mew_selected_building;
+        Log.d("DO Selected Building = ", selected_building);
+//        Log.d("DO Room = ", room);
+    }
 
 
     View.OnClickListener ActivityClickListener = new View.OnClickListener() {
@@ -183,10 +267,10 @@ public class TeacherAddClass extends AppCompatActivity implements AdapterView.On
             Toast.makeText(getApplicationContext(), "Please enter a valid subject title" , Toast.LENGTH_SHORT).show();
             return false;
         }
-        if (room_number.getText().toString().equals("")){
-            Toast.makeText(getApplicationContext(), "Please enter a room number" , Toast.LENGTH_SHORT).show();
-            return false;
-        }
+//        if (room_number.getText().toString().equals("")){
+//            Toast.makeText(getApplicationContext(), "Please enter a room number" , Toast.LENGTH_SHORT).show();
+//            return false;
+//        }
         if (chipGroup.getCheckedChipIds().size() == 0){
             Toast.makeText(getApplicationContext(), "Please select at least one day" , Toast.LENGTH_SHORT).show();
             return false;
@@ -207,8 +291,8 @@ public class TeacherAddClass extends AppCompatActivity implements AdapterView.On
             Chip chip = chipGroup.findViewById(id);
             days_of_the_week_selected.add(chip.getText().toString());
         }
-        String selectedRoom = room_number.getText().toString();
-
+//        String selectedRoom = room_number.getText().toString();
+        String selectedRoom = room;
         // Retrieve all documents in the collection
         collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -313,8 +397,9 @@ public class TeacherAddClass extends AppCompatActivity implements AdapterView.On
         class_information.put("END_HOUR", hour_end);
         class_information.put("END_MIN", min_end);
 
-        class_information.put("ROOM_NUMBER", room_number.getText().toString());
+//        class_information.put("ROOM_NUMBER", room_number.getText().toString());
 
+        class_information.put("ROOM_NUMBER", room);
         String full_class_name = subject_title.getText().toString() + " " + course_number.getText().toString();
 
         db.collection("COURSES")
