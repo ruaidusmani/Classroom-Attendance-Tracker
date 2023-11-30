210c9e4a3bce6c6cf7f06881ef9d6b8e5337f214
OurBoy22 <ramirez21ler@gmail.com>
2023-11-30 11:03:32 -0500

    preventing false marking of students in the same room

--- Module/test.py
+++ Module/test.py
@@ -227,12 +227,11 @@ def corroborateEmailWithTime(student, emails, hour, minute, course):
       arrival_hour =  dicto['PRESENT'][current_day + "_" + current_month + "_" + current_year][encoded_email]["arrival_hour"]
       arrival_minute =  dicto['PRESENT'][current_day + "_" + current_month + "_" + current_year][encoded_email]["arrival_minute"]
       try:
-        exit_hour =  dicto['PRESENT'][current_day + "_" + current_month + "_" + current_year][encoded_email]["exit_hour"] # will throw error if student did not exit
+        exit_hour =  dicto['PRESENT'][current_day + "_" + current_month + "_" + current_year][encoded_email]["exit_hour"] # will throw error if student did not exit class at any point
       except:
          exit_hour = "null"
       if (exit_hour != "null"):
          print("Student did not exit the class normally")
-         return "null"
       # START_HOUR = dicto["START_HOUR"]
       # START_MINUTE = dicto["START_MIN"]
       END_HOUR = dicto["END_HOUR"]
